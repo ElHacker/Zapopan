d50b69cfd13db457792e82f093e84e9f874d156c
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2013-05-04 17:40:30 -0500

    Initial setup for rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zapopan::Application.config.secret_token = 'ebadfaea2d20f96bfeaeb2f91d99089e3b49d2b5a927da0163b8127acec1af0b1c3676add4efb6f6f53f6ce1a334c4d812e134cc19c47915dbc26a223168786e'
